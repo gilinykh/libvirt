e146161faea7e145ea391ad3e706fb2a5a522d29
gilinykh <gleb.ilinykh@gmail.com>
2018-07-09 19:26:41 +0300

    initial commit

---
+++ src/main/java/com/example/libvirtapi/LibvirtApiApplication.java
@@ -0,0 +1,29 @@
+package com.example.libvirtapi;
+
+import org.libvirt.Connect;
+import org.libvirt.NodeInfo;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LibvirtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibvirtApiApplication.class, args);
+    }
+
+	@Bean
+	public CommandLineRunner createRunner() {
+
+		return args -> {
+			Connect conn = new Connect("qemu+ssh://root@vps3.intra/system");
+			NodeInfo nodeInfo = conn.nodeInfo();
+
+			System.out.println("model: " + nodeInfo.model + " mem(kb):" + nodeInfo.memory);
+
+			conn.close();
+		};
+	}
+}
